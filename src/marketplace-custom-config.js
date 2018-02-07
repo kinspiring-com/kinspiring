faf7672adfab9ee4ddb6c7a5cfdf24a2f3914063
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-02-07 14:04:06 +0200

    Update category config

--- src/marketplace-custom-config.js
+++ src/marketplace-custom-config.js
@@ -38,8 +38,14 @@ export const amenities = [
 ];
 
 export const categories = [
-  { key: 'smoke', label: 'Smoke' },
-  { key: 'electric', label: 'Electric' },
-  { key: 'wood', label: 'Wood' },
-  { key: 'other', label: 'Other' },
+  { key: 'matkarattaat', label: 'Matkarattaat' },
+  { key: 'yhdistelmarattaat', label: 'Yhdistelmärattaat' },
+  { key: 'tupla-ja-sisarusrattaat', label: 'Tupla- ja sisarusrattaat' },
+  { key: 'juoksurattaat', label: 'Juoksurattaat' },
+  { key: 'kantovalineet-ja-rinkat', label: 'Kantovälineet ja rinkat' },
+  { key: 'matkasangyt', label: 'Matkasängyt' },
+  { key: 'turvaistuimet', label: 'Turvaistuimet' },
+  { key: 'kuljetuslaukut-ja-vaunutarvikkeet', label: 'Kuljetuslaukut & vaunutarvikkeet' },
+  { key: 'pyoraily', label: 'Pyöräily' },
+  { key: 'muut-lastentarvikkeet', label: 'Muut lastentarvikkeet' },
 ];
