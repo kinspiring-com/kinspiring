1afc78f7ffe74ce46daa73955643af2be46dfe04
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-02-28 13:34:32 +0200

    Improve translation comparison script

--- compareTranslationKeys.js
+++ compareTranslationKeys.js
@@ -4,5 +4,14 @@ const fi = require('./src/translations/fi.json');
 const keysEn = Object.keys(en).sort();
 const keysFi = Object.keys(fi).sort();
 
-console.log('Keys in en.json', _.difference(keysEn, keysFi));
-console.log('Keys in fi.json', _.difference(keysFi, keysEn));
+const containsMarketplaceName = s => {
+  return s && s.toLowerCase().includes('kinspiring');
+};
+
+const missingKeys = _.difference(keysEn, keysFi);
+
+// Filter out keys that have a custom key.
+const extraKeys = _.difference(keysFi, keysEn).filter(s => !containsMarketplaceName(s));
+
+console.log('Keys missing from fi.json:', missingKeys);
+console.log('Extra keys in fi.json:', extraKeys);
