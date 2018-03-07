6699a2d9fefb6b957d378ba94a47c020097b3164
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-03-07 14:48:08 +0200

    Change about page content

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -15,18 +15,18 @@ import css from './AboutPage.css';
 import image from './about-us-1440.jpg';
 
 const AboutPage = () => {
-  const { siteTwitterHandle, siteFacebookPage } = config;
+  const { siteTwitterHandle, siteFacebookPage, siteInstagramPage } = config;
   const siteTwitterPage = twitterPageURL(siteTwitterHandle);
 
   // prettier-ignore
   return (
     <StaticPage
-      title="About Us"
+      title="Tietoa meistä"
       schema={{
         '@context': 'http://schema.org',
         '@type': 'AboutPage',
-        description: 'About Saunatime',
-        name: 'About page',
+        description: 'Tietoa Kinspiringista',
+        name: 'Tietoa meistä',
       }}
     >
       <LayoutSingleColumn>
@@ -35,53 +35,69 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>Experience the unique Finnish home sauna.</h1>
-          <img className={css.coverImage} src={image} alt="My first ice cream." />
+          <h1 className={css.pageTitle}>Vuokraa lastentarvikkeita turvallisesti</h1>
+          <img className={css.coverImage} src={image} alt="Heta ja Krista" />
 
           <div className={css.contentWrapper}>
-            <div className={css.contentSide}>
-              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
-            </div>
-
             <div className={css.contentMain}>
-              <h2>
-                Most of the Finnish saunas are located at the homes of individuals - indeed, most
-                people in Finland live in an apartment with sauna in it. In addition, lots of people
-                have lakeside summer cottages, which also typically come with a separate sauna
-                building near the waterfront.
-              </h2>
+              <h2>Kinspiringissa perheet vuokraavat lastentarvikkeita toisilleen turvallisesti.</h2>
 
               <p>
-                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
-                instead visit a real home or cottage sauna. Saunatime makes this possible for
-                everyone. All our saunas are owned by individuals willing to let tourists and other
-                curious visitors to enter their sacred spaces.
+                Kinspiring sai alkunsa kesällä 2016, kun Krista Pohjanlehdon kaksivuotiaan pojan
+                matkarattaat lojuivat jo toista viikkoa eteisen lattialla. Tahto-Otso ei ollut
+                suostunut istumaan laadukkaissa rattaissa pitkään aikaan, eikä pienirenkaisilla
+                rattailla loskasäässä olisi ajeltukaan.
               </p>
 
-              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
-
               <p>
-                Saunatime offers you a good way to earn some extra cash! If you're not using your
-                sauna every evening, why not rent it to other people while it's free. And even if
-                you are using your sauna every evening (we understand, it's so good), why not invite
-                other people to join you when the sauna is already warm! A shared sauna experience
-                is often a more fulfilling one.
+                – Ehkä Otso saisi jonain päivänä pikkusisaruksen ja rattaat pääsisivät taas
+                käyttöön, mutta mitä ihmettä me nyt niillä teemme? Krista mietti.
               </p>
 
-              <h3 id="contact" className={css.subtitle}>
-                Create your own marketplace like Saunatime
-              </h3>
               <p>
-                Saunatime is brought to you by the good folks at{' '}
-                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
-                to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
-                app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
-                get in touch!
+                Krista avasi tietokoneen ja teki rattaista vuokrausilmoituksen lapsiperheiden
+                Facebook-ryhmään. Puhelin piippasi taukoamatta seuraavina päivinä. Yksi halusi
+                vuokrata matkarattaat kahden viikon etelänlomaa varten ja toinen Ruotsinlaivalle.
+                Kolmas tahtoi testiajaa rattaat ennen ostopäätöksen tekemistä.
               </p>
+
               <p>
-                You can also checkout our{' '}
-                <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
-                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>.
+                Suuri kysyntä sai Kristan pohtimaan, miten perheet voisivat vuokrata
+                lastentarvikkeita toisilleen turvallisesti ja helposti.
+              </p>
+
+              <p>
+                – Täytyyhän muillakin perheille olla samoja haasteita? Monien lastentarvikkeiden
+                tarve on kuitenkin kausiluontoista ja lyhytaikaista, Krista pohti.
+              </p>
+
+              <p>
+                Ajatuksesta inspiroituneena Krista perusti muutamaa viikkoa myöhemmin Kinspiringin.
+              </p>
+
+              <p>
+                Tammikuussa 2017 Otson tuleva pikkusisarus potki iloisesti Kristan vatsassa.
+                Toisella puolella Lauttasaarta Heta Kärki katseli onnellisena nukkuvaa
+                Eeli-vauvaansa. Markkinointialalta tutun Kristan perustama Kinspiring innosti häntä:
+                hän näki Kinspiringissa voimaa tuomaan lapsiperheitä yhteen jakamaan tavaroiden
+                lisäksi myös kokemuksia.
+              </p>
+
+              <p>Pian Kristasta ja Hetasta tuli Kinspiring-tiimi.</p>
+
+              <p>
+                Me haluamme, että jokaisella perheellä on mahdollisuus käyttää laadukkaita
+                lastentarvikkeita ja uskomme jakamistalouden ratkaisevat monia lapsiperheiden
+                haasteita. Antamalla lastentarvikkeesi vuokralle tienaat ja annat muidenkin nauttia
+                hyvästä laadusta. Vuokraamalla tarpeeseen edistät kestävää kehitystä ja säästät
+                rahaa.
+              </p>
+
+              <p>
+                Voit rekisteröityä Kinspiringiin täällä. Löydät meidät myös{' '}
+                <ExternalLink href={siteFacebookPage}>Facebookissa</ExternalLink>,{' '}
+                <ExternalLink href={siteInstagramPage}>Instagramissa</ExternalLink> ja{' '}
+                <ExternalLink href={siteTwitterPage}>Twitterissä</ExternalLink>.
               </p>
             </div>
           </div>
