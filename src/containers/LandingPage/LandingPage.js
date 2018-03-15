610e634d6589e47b92dfe466395124e93ea7f246
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-03-15 16:52:15 +0200

    Fix share images
    - Remove extra slash from schema image
    - Use absolute URL in twitter image
    - Update twitter image dimensions

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -33,7 +33,7 @@ export const LandingPageComponent = props => {
   const siteTitle = config.siteTitle;
   const schemaTitle = intl.formatMessage({ id: 'LandingPage.schemaTitle' }, { siteTitle });
   const schemaDescription = intl.formatMessage({ id: 'LandingPage.schemaDescription' });
-  const schemaImage = `${config.canonicalRootURL}/${facebookImage}`;
+  const schemaImage = `${config.canonicalRootURL}${facebookImage}`;
 
   return (
     <Page
@@ -43,7 +43,9 @@ export const LandingPageComponent = props => {
       description={schemaDescription}
       title={schemaTitle}
       facebookImages={[{ url: facebookImage, width: 1200, height: 630 }]}
-      twitterImages={[{ url: twitterImage, width: 600, height: 314 }]}
+      twitterImages={[
+        { url: `${config.canonicalRootURL}${twitterImage}`, width: 1024, height: 512 },
+      ]}
       schema={{
         '@context': 'http://schema.org',
         '@type': 'WebPage',
